79d203a30396a83ffb7cf360d8fb99fe7ae7222d
Tina Tarighian <tt1@wellesley.edu>
2025-07-17 10:14:17 -0400

    Fix: Restore corrupted image paths and convert uploaded PNG to WebP
    - Fixed all /imagesimages/ double paths back to /images/
    - Restored empty image arrays to contain proper image paths
    - Converted uploaded screenshot from PNG to WebP format
    - Updated paths to use new WebP image location

--- tina/__generated__/client.d.ts
+++ tina/__generated__/client.d.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/tarighian/newTinaZone/ttarigh.github.io/tina/__generated__/.cache/1752750037419', url: 'https://content.tinajs.io/1.6/content/1b05e708-d9d0-4fd7-83ac-3ecf5ec12662/github/main', token: 'e3805f71f7d392dcc59d76da9b102427ad48a473', queries,  });
+export const client = createClient({ cacheDir: '/Users/tarighian/newTinaZone/ttarigh.github.io/tina/__generated__/.cache/1752753051994', url: 'https://content.tinajs.io/1.6/content/1b05e708-d9d0-4fd7-83ac-3ecf5ec12662/github/main', token: 'e3805f71f7d392dcc59d76da9b102427ad48a473', queries,  });
 export default client;
   
