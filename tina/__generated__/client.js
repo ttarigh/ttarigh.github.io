ec73fb4f17e2f4ec971d250cd9b238847cf967ab
Tina Tarighian <tt1@wellesley.edu>
2025-07-18 13:37:49 -0400

    finally sync

--- tina/__generated__/client.js
+++ tina/__generated__/client.js
@@ -1,4 +1,4 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: "/Users/tarighian/newTinaZone/ttarigh.github.io/tina/__generated__/.cache/1752859954543", url: "https://content.tinajs.io/1.6/content/1b05e708-d9d0-4fd7-83ac-3ecf5ec12662/github/main", token: "e3805f71f7d392dcc59d76da9b102427ad48a473", queries });
+export const client = createClient({ cacheDir: "/Users/tarighian/newTinaZone/ttarigh.github.io/tina/__generated__/.cache/1752860215980", url: "https://content.tinajs.io/1.6/content/1b05e708-d9d0-4fd7-83ac-3ecf5ec12662/github/main", token: "e3805f71f7d392dcc59d76da9b102427ad48a473", queries });
 export default client;
